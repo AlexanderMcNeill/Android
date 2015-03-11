1ba2d519bf845394f0492a2277598cbe51ea62b8
AlexanderMcNeill <mcnear1@student.op.ac.nz>
2015-03-11 22:13:14 +1300

    Finished the Hierachical activities lab

---
+++ Eclipse_Android/HierarchicalActivitiesv2/src/bit/mcnear1/hierarchicalactivitiesv2/NavListClickHandler.java
@@ -0,0 +1,8 @@
+package bit.mcnear1.hierarchicalactivitiesv2;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+
+
