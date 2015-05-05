a8e2b607204b3b5a935dc7df18bc783b1bca55f8
AlexanderMcNeill <mcnear1@student.op.ac.nz>
2015-05-05 22:48:08 +1200

    Added database model and creation sql for the final project

---
+++ documents/create_agile_manager_db.sql
@@ -0,0 +1,78 @@
+drop database if exists mcnear1_agile_manager;
+create database mcnear1_agile_manager;
+
+use mcnear1_agile_manager;
+
+create table Member(
+id int not null,
+firstName varchar(30) not null,
+lastName varchar(30) not null,
+userName varchar(30) unique not null,
+pass varchar(50) not null,
+
+primary key(id)
+) engine=INNODB;
+
+create table Team(
+id int not null,
+name varchar(30) unique not null,
+pass varchar(50) not null,
+
+primary key(id)
+) engine=INNODB;
+
+create table TeamMember(
+	memberID int not null,
+	teamID int not null,
+	
+	primary key(memberID, teamID),
+
+	foreign key(memberID)
+	references Member(id),
+
+	foreign key(teamID)
+	references Team(id)
+) engine=INNODB;
+
+create table ScrumMeeting(
+	id int not null,
+	teamID int not null,
+	date date not null,
+	barriersRemoved varchar(255),
+	collabOpportunities varchar(255),
+
+	foreign key(teamID)
+	references Team(id),	
+
+	primary key(id)
+) engine=INNODB;
+
+create table MemberScrum(
+	id int not null,
+	scrumMeetingID int not null,
+	memberID int not null,
+
+	obsticles varchar(255),
+
+	foreign key(scrumMeetingID)
+	references ScrumMeeting(id),
+
+	foreign key(memberID)
+	references Member(id),
+
+	primary key(id)
+) engine=INNODB;
+
+create table Goal(
+	id int not null,
+	memberScrumID int not null,
+
+	description varchar(255) not null,
+	achieved boolean not null,
+	comment varchar(255) not null,
+
+	foreign key(memberScrumID)
+	references MemberScrum(id),
+
+	primary key(id)
+) engine=INNODB;
